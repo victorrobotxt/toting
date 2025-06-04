7241e7e95de4356072a72001c79878e6e083dac0
victorrobotxt <139061689+victorrobotxt@users.noreply.github.com>
2025-06-04 07:51:47 +0300

    feat(frontend): add theme toggle

--- packages/frontend/src/pages/index.jsx
+++ packages/frontend/src/pages/index.jsx
@@ -1,6 +1,9 @@
+import ThemeToggle from '../components/ThemeToggle';
+
 export default function Home() {
   return (
-    <main style={{ display:'flex',alignItems:'center',justifyContent:'center',height:'100vh' }}>
+    <main style={{ display:'flex',alignItems:'center',justifyContent:'center',height:'100vh', gap:'1rem' }}>
+      <ThemeToggle />
       <a href="http://localhost:8000/auth/initiate">Log in with eID</a>
     </main>
   )
